9a2ec7d86ffc36351e9019cca19041adc63dd746
Leonardo Moreira <leonardo.moreira@ingresso.com>
2020-01-10 11:39:49 -0300

    code refactor

--- next.config.js
+++ next.config.js
@@ -9,10 +9,19 @@ module.exports = withCSS({
         use: {
           loader: 'url-loader',
           options: {
-            limit: 100000
+            limit: 100000,
+            quality: 85
           }
         }
-      })
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'image-webpack-loader',
+        // Specify enforce: 'pre' to apply the loader
+        // and not duplicate it in rules with them
+        enforce: 'pre'
+      }
+    )
     return config
   }
 })
--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,6 @@ import Head from 'next/head';
 import App from 'next/app';
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
 
