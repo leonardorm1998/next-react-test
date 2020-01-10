f6325965a2de5ff4ec6bc77a40a312c8b7f7ad56
Leonardo Moreira <leonardo.moreira@ingresso.com>
2020-01-10 13:00:53 -0300

    adjusts next.config.js to css and images

--- next.config.js
+++ next.config.js
@@ -1,25 +1,47 @@
 const withCSS = require('@zeit/next-css');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 
 module.exports = withCSS({
+  distDir: '.build',
   cssModules: false,
   webpack: (config) => {
+    if (config.mode === 'production') {
+      if (Array.isArray(config.optimization.minimizer)) {
+        config.optimization.minimizer.push(
+          new OptimizeCSSAssetsPlugin({})
+        )
+      }
+    }
     config.module.rules.push(
       {
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-            quality: 85
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
           }
-        }
-      },
-      {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        loader: 'image-webpack-loader',
-        // Specify enforce: 'pre' to apply the loader
-        // and not duplicate it in rules with them
-        enforce: 'pre'
+        ]
       }
     )
     return config
