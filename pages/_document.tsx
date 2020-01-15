8cafa65cc55a301d22cc91c91fe6e3ba28f13582
Leonardo Moreira <leonardo.moreira@ingresso.com>
2020-01-15 11:37:36 -0300

    change path of favicon.ico on _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,7 +21,7 @@ class MyDocument extends Document {
           <meta name='theme-color' content='#2196F3' />
           <meta name='description' content='Progressive Web App made with Next.js' />
           <link rel='manifest' href='/manifest.json' />
-          <link rel='shortcut icon' href='/public/icons/favicon.ico' />
+          <link rel='shortcut icon' href='/icons/favicon.ico' />
         </Head>
         <body>
           <noscript>Your browser does not support JavaScript!</noscript>
